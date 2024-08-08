2fbeb07fca4cfd8f3d1295c826b0acfb8567373f
my-rice <dvdrsi2017@gmail.com>
2024-08-08 11:00:03 +0200

    [FIX] fixed small visibility bug

--- tdmpc2/envs/robots/UnitreeH1.py
+++ tdmpc2/envs/robots/UnitreeH1.py
@@ -142,13 +142,13 @@ class H1(Robot):
         # Skip the 7 DoFs of the free root joint.
         return self._env.data.qpos[7 : self.dof].copy() # OK
 
-    def robot_orientation(self):
-        """Returns the orientation of the robot."""
-        return self._env.data.qpos[3:7].copy()
-
     def joint_velocities(self):
         """Returns the joint velocities."""
         return self._env.data.qvel[6 : self.dof].copy() # OK
+    
+    def robot_orientation(self):
+        """Returns the orientation of the robot."""
+        return self._env.data.qpos[3:7].copy()
 
     def control(self):
         """Returns a copy of the control signals for the actuators."""
--- tdmpc2/envs/robots/__init__.py
+++ tdmpc2/envs/robots/__init__.py
@@ -1,1 +1,2 @@
 from .UnitreeH1 import H1
+from robot import Robot
